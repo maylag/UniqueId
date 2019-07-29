185743c989cce1549db19fdd03f609dd972519fb
Fankx <fankx@startimes.com.cn>
2019-07-29 17:54:19 +0800

    Sequences

--- RayCore/src/main/java/com/star/meta/Sequences.java
+++ RayCore/src/main/java/com/star/meta/Sequences.java
@@ -1,5 +1,7 @@
 package com.star.meta;
 
+import com.star.constant.MetaConstants;
+
 import java.util.concurrent.atomic.AtomicReference;
 
 public final class Sequences {
@@ -17,7 +19,32 @@ public final class Sequences {
                 }
             }
         }
-        //TODO
-        return null;
+        AtomicReference<TimeStampAndSequence> atomicReference = atomicReferences[index];
+        TimeStampAndSequence timeStampAndSequence = atomicReference.get();
+        TimeStampAndSequence newTimeStampAndSequence = buildNewTimeStampAndSequence(timeStamp, timeStampAndSequence);
+        if (atomicReference.compareAndSet(timeStampAndSequence, newTimeStampAndSequence)) {
+            return newTimeStampAndSequence;
+        } else {
+            return getSequence(index, timeStamp);
+        }
+
+        // 之后需要同步timeStamp
+    }
+
+    private TimeStampAndSequence buildNewTimeStampAndSequence(long timeStamp, TimeStampAndSequence timeStampAndSequence) {
+        if (timeStamp == timeStampAndSequence.getTimestamp()) {
+            return buildAppendTimeStampAndSequence(timeStamp, timeStampAndSequence);
+        } else if (timeStamp > timeStampAndSequence.getTimestamp()) {
+            return new TimeStampAndSequence(timeStamp, 0);
+        } else {
+            return buildAppendTimeStampAndSequence(timeStampAndSequence.getTimestamp(), timeStampAndSequence);
+        }
+    }
+
+    private TimeStampAndSequence buildAppendTimeStampAndSequence(long timeStamp, TimeStampAndSequence timeStampAndSequence) {
+        long currentSequence = timeStampAndSequence.getSequence() + 1;
+        return currentSequence > MetaConstants.SEQUENCE_MASK
+                ? new TimeStampAndSequence(timeStamp + 1, 0)
+                : new TimeStampAndSequence(timeStamp, currentSequence);
     }
 }
